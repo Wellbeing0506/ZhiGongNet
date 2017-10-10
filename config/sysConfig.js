462eeba6cdde173ed3aa338d3a96d0b2dfe2276c
dave <well1001@hotmail.com>
2017-10-10 18:45:58 +0800

    modify redis ip

--- config/sysConfig.js
+++ config/sysConfig.js
@@ -9,7 +9,7 @@ module.exports = function(){
 			}};
 	} else if (process.env.NODE_ENV==='production') {
 			return {redis : {
-				ip : "172.31.23.229",
+				ip : "172.30.178.99",
 				port : 6379,
 				option : {auth_pass:'qwe123QWE!@#'},
 				keyHead : "ZhiGongNet"
